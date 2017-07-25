2afa98052658c807caa7ae8f9524419d2d854942
DmitriyIndigo <dima.sverdloff@mail.ru>
2017-07-25 18:57:21 +0300

    Update tables.sql

--- DB/tables.sql
+++ DB/tables.sql
@@ -1,15 +1,9 @@
-База данных: `CyberPlat`
-
-Структура таблицы `Users`
-
 CREATE TABLE `Users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`email`)
 );
-
-Структура таблицы `sessions`
 
 CREATE TABLE `sessions` (
   `id` char(32) NOT NULL,
